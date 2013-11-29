d16a1eaa8957c80ef9399496096ce747834a57c3
Malte Schmitz <malte@schmitz-sh.de>
2013-11-29 13:17:59 +0100

    Adds initial please wait message to canvas

--- slideshow.js
+++ slideshow.js
@@ -67,6 +67,14 @@ $(function () {
     }
   });
 
+  // display initial please wait on the canvas
+  $canvas[0].height = 544;
+  $canvas[0].width = 725;
+  context.font = "24pt \"Helvetica Neue\",Helvetica,Arial,sans-serif";
+  context.fillText("Bitte warten!", 10, 50);
+  context.font = "16pt \"Helvetica Neue\",Helvetica,Arial,sans-serif";
+  context.fillText("Präsentation wird geladen...", 10, 100);
+
   player.jPlayer({
     ready: function () {
       $(this).jPlayer("setMedia", {
@@ -162,6 +170,9 @@ $(function () {
     // Hide pointer
     slideshowPointer.hide();
   }
+
+  // Set Path to worker
+  PDFJS.workerSrc = 'libs/pdf.worker.js';
 
   // Asynchronously download PDF as an ArrayBuffer
   PDFJS.getDocument("//slideshow/latex.pdf").then(function (_pdfDoc) {
